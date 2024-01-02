9fa5de9afac74dd536c5972c0f18fd2ede8a4da2
CyberMedina <jhonatanmedina5255@gmail.com>
2024-01-01 19:06:22 -0600

    Inicializando sqlalchemy

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR, text
+from dotenv import load_dotenv
+from models import *
+load_dotenv()
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine= create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True,
+        connect_args={
+            "keepalives": 1,
+            "keepalives_idle": 30,
+            "keepalives_interval": 10,
+            "keepalives_count": 5,
+        }
+)
+db = scoped_session(sessionmaker(bind=engine))
+
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
  
