bc88fcd1a9aebb7cb1d844f5787c861f6fe42063
CyberMedina <jhonatanmedina5255@gmail.com>
2024-07-25 21:28:02 -0600

    añadiendo pool_pre_ping para la base de datos

--- db.py
+++ db.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True)
 db_session = scoped_session(sessionmaker(bind=engine))
