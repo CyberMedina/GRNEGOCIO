fda9cc2fae3334b70b1d19bb75f6c22f812d7b51
CyberMedina <jhonatanmedina5255@gmail.com>
2024-12-09 22:33:49 -0600

    fix api obtener clientes pagados v3

--- models/API_Alexa.py
+++ models/API_Alexa.py
@@ -574,44 +574,25 @@ def crear_cadena_respuesta_obtener_pago_normal(db_session, nombre_cliente):
     print("Creando cadena de respuesta...")
     print(nombre_cliente)
     
-
     # Obtener ID del cliente por nombre
     id_cliente = seleccionar_clientes_activos(db_session, nombre_cliente)
-
     datos_cliente = seleccionar_datos_cliente(db_session, id_cliente)
-
     pago = obtener_pagoEspecial(db_session, id_cliente, datetime.datetime.now())
 
     if pago['estado'] == 2:
-
         nombres_apellidos_cliente = f"{datos_cliente[1]} {datos_cliente[2]}"
         pago['nombres_apellidos_cliente'] = nombres_apellidos_cliente
         pago['id_cliente'] = id_cliente
-
-
         pago['cifra_cordobas'] = pago['cifra'] * obtener_tasa_cambio_local()['cifraTasaCambio']
-
         pago['tasa_cambio'] = obtener_tasa_cambio_local()['cifraTasaCambio']
+        # Additional processing for estado == 2
 
-    # Convertir Decimal a float
-        if 'cifra' in pago and isinstance(pago['cifra'], Decimal):
-            pago['cifra'] = float(pago['cifra'])
-
-        if 'cifra_cordobas' in pago and isinstance(pago['cifra_cordobas'], Decimal):
-            pago['cifra_cordobas'] = float(pago['cifra_cordobas'])
-            pago['cifra_cordobas'] = math.ceil(pago['cifra_cordobas'])
-
-        if 'tasa_cambio' in pago and isinstance(pago['tasa_cambio'], Decimal):
-            pago['tasa_cambio'] = float(pago['tasa_cambio'])
-        
-
-        
-
-
-    
-
-    
+    # Convertir todos los Decimals a float
+    for key, value in pago.items():
+        if isinstance(value, Decimal):
+            pago[key] = float(value)
+            if key == 'cifra_cordobas':
+                pago[key] = math.ceil(pago[key])
 
     print(pago)
-
     return pago
