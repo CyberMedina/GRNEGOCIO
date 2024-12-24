19d6576fd488fd4a5dcde3d92d6185a23567be63
CyberMedina <jhonatanmedina5255@gmail.com>
2024-12-23 23:58:46 -0600

    fix login

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 
-password = 'AmazoAlexan12334!'
+password = 'ChihiroMuñeca1234!'
 
 hashed_password = generate_password_hash(password)
 
